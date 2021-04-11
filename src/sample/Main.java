5690b7e267c3de8252ff8a7b5d2e512516238e6b
MoskvichevViktor <beldetal@yandex.ru>
2021-04-11 12:47:43 +0300

    baza

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,28 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("sample.fxml"));
+        Parent root = loader.load();
+        primaryStage.setTitle("Chat 2021");
+        primaryStage.setScene(new Scene(root, 600, 450));
         primaryStage.show();
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            public void handle(WindowEvent we) {
+                Controller controller = loader.getController();
+                controller.disconnect();
+            }
+        });
     }
 
 
